32b7ef222d056a357688c839414f6665ea1882ff
SS47816 <ss1057532022@gmail.com>
2020-12-13 23:22:41 +0800

    fix: fixing Matrix size issue

--- Homeworks/HW2/odom_ws/src/calib_odom/src/Odom_Calib.cpp
+++ Homeworks/HW2/odom_ws/src/calib_odom/src/Odom_Calib.cpp
@@ -5,7 +5,7 @@
 void OdomCalib::Set_data_len(int len)
 {
     data_len = len;
-    A.conservativeResize(len*3,9);
+    A.conservativeResize(len*3, 9);
     b.conservativeResize(len*3);
     A.setZero();
     b.setZero();
@@ -26,14 +26,31 @@ bool OdomCalib::Add_Data(Eigen::Vector3d Odom,Eigen::Vector3d scan)
     if(now_len < INT_MAX)
     {
         //TODO: 构建超定方程组
-        A = Eigen::Matrix3d();
-        A << Odom, Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero(), 
-             Eigen::Vector3d::Zero(), Odom, Eigen::Vector3d::Zero(),
-             Eigen::Vector3d::Zero(), Eigen::Vector3d::Zero(), Odom;
-        std::cout << "Matrix A: " << std::endl;
-        std::cout << A << std::endl;
+        // Eigen::Matrix3d A_i;
+        // A << Odom.transpose(), Eigen::RowVector3d::Zero(), Eigen::RowVector3d::Zero(), 
+        //      Eigen::RowVector3d::Zero(), Odom.transpose(), Eigen::RowVector3d::Zero(),
+        //      Eigen::RowVector3d::Zero(), Eigen::RowVector3d::Zero(), Odom.transpose();
+        // A << A_i;
+        Eigen::MatrixXd A_i(3, 9);
+        std::cout << "Adding to Matrix A..." << std::endl;
+        A_i << Odom(0), Odom(1), Odom(2), 0, 0, 0, 0, 0, 0,
+               0, 0, 0, Odom(0), Odom(1), Odom(2), 0, 0, 0,
+               0, 0, 0, 0, 0, 0, Odom(0), Odom(1), Odom(2);
+        std::cout << "Adding to Matrix A..." << std::endl;
+        A.conservativeResize(A.rows()+3, A.cols());
+        std::cout << "Adding to Matrix A..." << std::endl;
+        A.bottomRows(3) = A_i;
 
-        b = scan;
+        std::cout << "No. of lens: " << now_len << " & " << data_len << std::endl;
+        std::cout << "Matrix A is of size " << A.rows() << "x" << A.cols() << std::endl;
+        // std::cout << A << std::endl;
+
+        std::cout << "Adding to Vector b..." << std::endl;
+        b.conservativeResize(b.rows()+1, A.cols());
+        b.bottomRows(1) = scan;
+        std::cout << "Vector b is of size " << b.rows() << "x" << b.cols() << std::endl;
+        // std::cout << b.transpose() << std::endl;
+
         //end of TODO
         now_len++;
         return true;
@@ -54,7 +71,7 @@ Eigen::Matrix3d OdomCalib::Solve()
     Eigen::Matrix3d correct_matrix;
 
     //TODO: 求解线性最小二乘
-    
+    // correct_matrix = A.ldlt().solve(b);
     //end of TODO
 
     return correct_matrix;
@@ -65,7 +82,7 @@ Eigen::Matrix3d OdomCalib::Solve()
 */
 bool OdomCalib::is_full()
 {
-    if(now_len%data_len==0&&now_len>=1)
+    if(now_len%data_len == 0 && now_len >= 1)
     {
         now_len = data_len;
         return true;
