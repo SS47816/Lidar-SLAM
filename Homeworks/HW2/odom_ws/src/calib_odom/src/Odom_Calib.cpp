450dfcf3124576fa25ae0e6923092ca32c6b4a9f
SS47816 <ss1057532022@gmail.com>
2020-12-13 23:33:04 +0800

    fix: complied successfully

--- Homeworks/HW2/odom_ws/src/calib_odom/src/Odom_Calib.cpp
+++ Homeworks/HW2/odom_ws/src/calib_odom/src/Odom_Calib.cpp
@@ -26,30 +26,19 @@ bool OdomCalib::Add_Data(Eigen::Vector3d Odom,Eigen::Vector3d scan)
     if(now_len < INT_MAX)
     {
         //TODO: 构建超定方程组
-        // Eigen::Matrix3d A_i;
-        // A << Odom.transpose(), Eigen::RowVector3d::Zero(), Eigen::RowVector3d::Zero(), 
-        //      Eigen::RowVector3d::Zero(), Odom.transpose(), Eigen::RowVector3d::Zero(),
-        //      Eigen::RowVector3d::Zero(), Eigen::RowVector3d::Zero(), Odom.transpose();
-        // A << A_i;
         Eigen::MatrixXd A_i(3, 9);
-        std::cout << "Adding to Matrix A..." << std::endl;
         A_i << Odom(0), Odom(1), Odom(2), 0, 0, 0, 0, 0, 0,
                0, 0, 0, Odom(0), Odom(1), Odom(2), 0, 0, 0,
                0, 0, 0, 0, 0, 0, Odom(0), Odom(1), Odom(2);
-        std::cout << "Adding to Matrix A..." << std::endl;
         A.conservativeResize(A.rows()+3, A.cols());
-        std::cout << "Adding to Matrix A..." << std::endl;
         A.bottomRows(3) = A_i;
 
-        std::cout << "No. of lens: " << now_len << " & " << data_len << std::endl;
+        std::cout << "No. of lens: " << now_len << " / " << data_len << std::endl;
         std::cout << "Matrix A is of size " << A.rows() << "x" << A.cols() << std::endl;
-        // std::cout << A << std::endl;
 
-        std::cout << "Adding to Vector b..." << std::endl;
-        b.conservativeResize(b.rows()+1, A.cols());
-        b.bottomRows(1) = scan;
+        b.conservativeResize(b.rows()+3, b.cols());
+        b.bottomRows(3) = scan;
         std::cout << "Vector b is of size " << b.rows() << "x" << b.cols() << std::endl;
-        // std::cout << b.transpose() << std::endl;
 
         //end of TODO
         now_len++;
@@ -71,7 +60,9 @@ Eigen::Matrix3d OdomCalib::Solve()
     Eigen::Matrix3d correct_matrix;
 
     //TODO: 求解线性最小二乘
-    // correct_matrix = A.ldlt().solve(b);
+    Eigen::VectorXd result(9);
+    result = A.ldlt().solve(b);
+    correct_matrix << result;
     //end of TODO
 
     return correct_matrix;
